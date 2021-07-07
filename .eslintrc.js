36fa5f7501aa6e0c860661eae40159e02212e28a
JrzenonDev <jrobertoonb@gmail.com>
2021-07-07 00:04:02 -0300

    chore: more configuration initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,19 @@ module.exports = {
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'import/first': 'off',
+    'import/order': 'off',
+    indent: 'off',
+    '@typescript-eslint/indent': [
+      'error',
+      'space',
+    ],
+    'no-new': 'off',
+    'no-tabs': 'off',
+    semi: ['error', 'always'],
+    'vue/html-indent': ['error', 'tab'],
+    'vue/order-in-components': 'off',
+  }
 }
