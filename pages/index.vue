05cf45c9154459f972c1e6c57aca2f81180dad0d
JrzenonDev <jrobertoonb@gmail.com>
2021-07-08 14:30:16 -0300

    chore: starting component CountDown

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,43 @@
     <div class="flex flex-col w-full ld:w-1/2">
       <Profile />
       <CompletedChallenges />
+      <Countdown @completed="getNewChallenge" />
+
+      <button
+        v-if="hasCountDownCompleted"
+        disabled
+        class="button completed"
+      >
+        Cycle completed
+      </button>
+      <button
+        v-else-if="isCountDownActive"
+        class="button abadon"
+        @click="setCountDownState(false)"
+      >
+        Abandon cycle
+      </button>
+      <button
+        v-else
+        class="button start"
+        @click="setCountDownState(true)"
+      >
+        Start a cycle
+      </button>
+
     </div>
   </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+
+import { mapState, mapMutations } from 'vuex'
+import { Mutations as CountDownMT } from '~/store/Countdown/types'
+
 import CompletedChallenges from '~/components/atoms/CompletedChallenge.vue';
 import Profile from '~/components/molecules/Profile.vue';
+import CountDown from '~/components/molecules/CountDown.vue';
 
 interface Head {
   title: string
@@ -24,6 +53,7 @@ export default Vue.extend({
   },
   components: {
     CompletedChallenges,
+    CountDown,
     Profile,
   }
 });
