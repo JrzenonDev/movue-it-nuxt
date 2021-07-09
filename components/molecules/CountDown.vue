9523d4fe4dc0db05dfff34409f34d68ead1c9a80
JrzenonDev <jrobertoonb@gmail.com>
2021-07-09 15:05:24 -0300

    chore: create molecule component countdown and molecule component countdowndigits with functions util split value

--- components/molecules/CountDown.vue
+++ components/molecules/CountDown.vue
@@ -1,11 +1,29 @@
 <template>
-
+  <div class="flex justify-center items-center mt-8 lg:mt-14 text-9xl text-title font-rajdhani">
+    <CountdownDigits :digits="minutes" />
+    <span class="bg-backgroung px-2">:</span>
+    <CountdownDigits :digits="seconds" />
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue';
 
-}
+import { mapState, mapGetters, mapMutations } from 'vuex';
+import {  } from '~/store/Countdown/types';
+
+import CountdownDigits from '~/components/atoms/CountdownDigits.vue';
+
+export default Vue.extend({
+  components: { CountdownDigits },
+  computed: {
+    ...mapState('Countdown', ['time', 'isActive']),
+    ...mapGetters('Countdown', ['minutes', 'seconds']),
+  },
+  methods: {
+
+  }
+});
 </script>
 
 <style>
